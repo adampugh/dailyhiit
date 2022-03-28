631a0dd9186a4b606d6964210d1d966d640315b3
Adam <adampugh@zoho.com>
2022-03-28 02:26:57 +0100

    fix safari resize observer bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AuthUserProvider } from '../context/AuthUserContext';
 import { UserContextProvider } from '../context/UserContext';
 import '../styles/globals.css';
+import { install } from 'resize-observer';
 import type { AppProps } from 'next/app';
+
+if (typeof window !== 'undefined') {
+    if (!window.ResizeObserver) install();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
